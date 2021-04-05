1d37da0e1259ce44b9a5dc666e81a598f6f9a349
kakakikikeke <cdl.yoshinaka@gmail.com>
2021-04-05 14:20:48 +0900

    Update metadata

--- helm_chart/build.sh
+++ helm_chart/build.sh
@@ -1,4 +1,6 @@
 VERSION=`grep 'version:' ./request-dumper/Chart.yaml | cut -d' ' -f2`
+cp ../README.md ./request-dumper/
 helm package ./request-dumper
 mv request-dumper-${VERSION}.tgz ../docs
 helm repo index ../docs --url http://kakakikikeke.github.io/request-dumper
+rm ./request-dumper/README.md
